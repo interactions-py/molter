9bb432ac6194f5e4813214d6874876cf5f8dee03
Astrea49 <25420078+Astrea49@users.noreply.github.com>
2022-07-11 20:03:04 -0400

    docs: add examples for handling errors

--- examples/advanced/bot.py
+++ examples/advanced/bot.py
@@ -97,4 +97,5 @@ async def user_starts_with_a(ctx: molter.MolterContext):
 
 # Loading the other files for this example, don't mind me.
 client.load("extension")
+client.load("handling_errors")
 client.start()
